0b00ffc988654c43ed1205633daef538b961ca48
vaneiker <39521031+vaneiker@users.noreply.github.com>
2019-07-17 17:02:22 -0400

    ver
    add

---
+++ CapaLogicaNegocio/Global.js
@@ -0,0 +1,36 @@
+﻿
+
+$(document).ready(function () {
+
+});
+
+function obtenerArray() {
+    var lines = $('#textarea').val().split(/\n/);
+    var texts = []
+    for (var i = 0; i < lines.length; i++) {
+        // only push this line if it contains a non whitespace character.
+        if (/\S/.test(lines[i])) {
+            texts.push($.trim(lines[i]));
+        }
+    }
+    let t = JSON.stringify(texts)
+    alert(t)
+
+    ValidateLoggin(t);
+}
+
+function ValidateLoggin(t) {
+    $.ajax({
+        type: "POST",
+        url: "Home/CreateUserAgentNew",
+        data: t,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json", 
+        success: function (data) {
+            alert(data)
+        },
+        error: function (data) {
+            alert(data)
+        }
+    });
+}
---
+++ CapaLogicaNegocio/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace System.FrontEnd.Agent_Users.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult CreateUserAgentNew(List<string> codigos)
+        {
+            dynamic showMessageString = string.Empty;
+            //Logica
+            showMessageString = new
+            {
+                param1 = "Success",
+                param2 = "Usuarios Creados Exitosamente",
+                
+            };
+            //showMessageString = new
+            //{
+            //    param1 = "Error",
+            //    param2 = "No se Creo Ningún Usuario",
+            //    //param3 = resp.ErrorMenssager
+            //};
+
+            return Json(showMessageString, JsonRequestBehavior.AllowGet);
+        } 
+    }
+}
